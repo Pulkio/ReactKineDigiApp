1dea054b1a5647cc3c6fb36fa08ecfa53c9b25ab
Pulkio <guillaume.cobat@gmx.fr>
2023-12-14 11:21:59 +0100

    changement graphique page accueil boutons

--- ReactKineDigiApp/app/screens/Accueil.tsx
+++ ReactKineDigiApp/app/screens/Accueil.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet, Image } from 'react-native';
 import { NavigationProp } from '@react-navigation/native';
 import { FIREBASE_AUTH } from '../../FirebaseConfig';
 
@@ -8,12 +8,10 @@ interface RouteProps {
 }
 
 const Accueil = ({ navigation }: RouteProps) => {
-  // Fonction pour naviguer vers la page AddPatient.tsx
   const goToAddPatient = () => {
     navigation.navigate('AddPatient');
   };
 
-  // Fonction pour naviguer vers la page DeletePatient.tsx
   const goToDeletePatient = () => {
     navigation.navigate('DeletePatient');
   };
@@ -33,13 +31,15 @@ const Accueil = ({ navigation }: RouteProps) => {
 
       {/* Contenu principal */}
       <View style={styles.content}>
-        <TouchableOpacity style={styles.button} onPress={goToAddPatient}>
+        <Image source={require('../../assets/logo_kine_app.png')} style={styles.logo} />
+
+        <TouchableOpacity style={[styles.button, styles.addPatientButton]} onPress={goToAddPatient}>
           <Text style={styles.buttonText}>Ajouter un patient</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.button} onPress={goToDeletePatient}>
+        <TouchableOpacity style={[styles.button, styles.deletePatientButton]} onPress={goToDeletePatient}>
           <Text style={styles.buttonText}>Supprimer un patient</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.button} onPress={goToTest}>
+        <TouchableOpacity style={[styles.button, styles.chooseTestButton]} onPress={goToTest}>
           <Text style={styles.buttonText}>Choisir un test</Text>
         </TouchableOpacity>
       </View>
@@ -48,43 +48,55 @@ const Accueil = ({ navigation }: RouteProps) => {
 };
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-    },
-    header: {
-      flexDirection: 'row',
-      justifyContent: 'flex-end', // Aligne le bouton "Logout" à droite
-      alignItems: 'center',
-      paddingHorizontal: 20,
-      paddingTop: 10,
-    },
-    headerText: {
-      fontSize: 18,
-      fontWeight: 'bold',
-    },
-    logoutButton: {
-      backgroundColor: '#007BFF',
-      padding: 10,
-      borderRadius: 5,
-    },
-    buttonText: {
-      color: '#fff',
-      fontSize: 16,
-    },
-    content: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
-    button: {
-      backgroundColor: '#007BFF',
-      padding: 15,
-      borderRadius: 10,
-      marginBottom: 20,
-      width: '80%',
-      alignItems: 'center',
-    },
-  });
+  container: {
+    flex: 1,
+    backgroundColor: '#F7F7F7', // Couleur de fond plus douce
+  },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+    paddingTop: 10,
+  },
+  logoutButton: {
+    backgroundColor: 'rgba(255, 59, 48, 0.8)', // Rouge avec opacité réduite
+    padding: 10,
+    borderRadius: 5,
+  },
+  content: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  logo: {
+    width: 200,
+    height: 200,
+    resizeMode: 'contain',
+    marginBottom: 20,
+  },
+  button: {
+    paddingVertical: 15,
+    paddingHorizontal: 40,
+    borderRadius: 10,
+    marginBottom: 20,
+    alignItems: 'center',
+    backgroundColor: '#EFEFEF', // Couleur de fond plus douce pour les boutons
+  },
+  buttonText: {
+    color: '#333', // Couleur de texte plus foncée
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  addPatientButton: {
+    backgroundColor: '#82C7A5', // Vert pastel
+  },
+  deletePatientButton: {
+    backgroundColor: '#F5A623', // Orange pastel
+  },
+  chooseTestButton: {
+    backgroundColor: '#5E9FFF', // Bleu pastel
+  },
+});
 
 export default Accueil;
